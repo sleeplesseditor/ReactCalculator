e255454dedd2d0c89db344e95bba19c999d9e9a5
sleeplesseditor <josh@bryson.me.uk>
2018-10-03 18:26:45 +0100

    Basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeyPadComponent';
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+        result: ""
+    }
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+        this.calculate()
+    }
+
+    else if(button === "C"){
+        this.reset()
+    }
+    else if(button === "CE"){
+        this.backspace()
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+  };
+
+  calculate = () => {
+    try {
+        this.setState({
+            // eslint-disable-next-line
+            result: (eval(this.state.result) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+
+    }
+  };
+
+  reset = () => {
+      this.setState({
+          result: ""
+      })
+  };
+
+  backspace = () => {
+      this.setState({
+          result: this.state.result.slice(0, -1)
+      })
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="calculator-body">
+          <h1>Simple Calculator</h1>
+          <ResultComponent result={this.state.result}/>
+          <KeyPadComponent onClick={this.onClick}/>
+        </div>
       </div>
     );
   }
